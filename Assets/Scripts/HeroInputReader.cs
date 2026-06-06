4c16930155d30104694461746764714c3ddf4547
Artyom <>
2026-06-06 18:08:01 +0300

    Set background

--- Assets/Scripts/HeroInputReader.cs
+++ Assets/Scripts/HeroInputReader.cs
@@ -9,7 +9,7 @@ public class HeroInputReader : MonoBehaviour
         _hero =  GetComponent<Hero>();
     }
 
-    public void OnHorizontalMovement(InputAction.CallbackContext context)
+    public void OnMovement(InputAction.CallbackContext context)
     {
         var direction = context.ReadValue<Vector2>();
         _hero.SetDirection(direction);
