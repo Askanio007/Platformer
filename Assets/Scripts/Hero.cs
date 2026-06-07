9a0cea1ebafc8fc210b2e0d33be3dd9b6f04aaea
Artyom <>
2026-06-07 17:25:13 +0300

    Implement jump

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,21 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Hero : MonoBehaviour
 {
-    private Vector2 _direction;
-
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private LayerMask _groundLayer;
 
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _groundCheckRadius;
+    [SerializeField] private Vector3 _groundCheckPositionDelta;
 
-    void Update()
+    private Vector2 _direction;
+    private Rigidbody2D _rigedbody;
+    
+
+    void Awake()
     {
-        var delta = _direction * _speed * Time.deltaTime;
-        transform.position = new Vector3(transform.position.x + delta.x, transform.position.y + delta.y, transform.position.z);
+        _rigedbody = GetComponent<Rigidbody2D>();
     }
 
     public void SetDirection(Vector2 direction)
@@ -23,5 +25,32 @@ public class Hero : MonoBehaviour
         _direction = direction;
     }
 
-    
+    void FixedUpdate()
+    {
+        _rigedbody.linearVelocity = new Vector2(_direction.x * _speed, _rigedbody.linearVelocity.y);
+        if (_direction.y > 0)
+        {
+            if (IsGrounded())
+            {
+                _rigedbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+        } 
+        else if (_rigedbody.linearVelocity.y > 0)
+        {
+            _rigedbody.linearVelocity = new Vector2(_rigedbody.linearVelocity.x, _rigedbody.linearVelocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        var hit = Physics2D.CircleCast(transform.position + _groundCheckPositionDelta, _groundCheckRadius, Vector2.down, 0, _groundLayer);
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = IsGrounded() ? Color.green : Color.red;
+        Gizmos.DrawSphere(transform.position + _groundCheckPositionDelta, _groundCheckRadius);
+    }
+
 }
